435f2de250e5ffa0cac4c2d282768e03af64b137
EC2 Default User <ec2-user@ip-172-31-36-193.ap-northeast-1.compute.internal>
2023-11-13 06:45:07 +0000

    [finish]課題提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,14 @@ class BooksController < ApplicationController
 
 # バリデーションを記述する
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -17,13 +22,17 @@ class BooksController < ApplicationController
   end
 
   def edit
+    flash[:notice] = "Book was successfully updated."
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
